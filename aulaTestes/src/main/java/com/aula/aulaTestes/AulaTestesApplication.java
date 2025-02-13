1b72eabee74c5e590e01e9715e04c12994adae2c
Emerson Andrey Fausto dos Santos <202225020017@ad.ifpb.edu.br>
2025-02-13 19:15:46 -0300

    Add: adicao inicial

---
+++ aulaTestes/src/main/java/com/aula/aulaTestes/AulaTestesApplication.java
@@ -0,0 +1,13 @@
+package com.aula.aulaTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaTestesApplication.class, args);
+	}
+
+}
