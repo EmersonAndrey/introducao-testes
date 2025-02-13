2cc50d4fd90aecd17ba7d4009876bddc3ff74478
Emerson Andrey Fausto dos Santos <202225020017@ad.ifpb.edu.br>
2025-02-13 20:31:43 -0300

    Preparando ambiente

---
+++ aulaTestes/src/main/java/com/aula/aulaTestes/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.aula.aulaTestes.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/get")
+    public String getUser() {
+        return "Carregando usuário...";
+    }
+
+    @PostMapping("/save")
+    public String saveUser() {
+        return "Salvando usuário...";
+    }
+
+    @PutMapping("/update")
+    public String updateUser() {
+        return "Atualizando usuário...";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteUser() {
+        return "Deletando usuário...";
+    }
+    
+}
